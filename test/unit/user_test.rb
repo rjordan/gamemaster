978aa3b8e62571bf3d03f1e83d4b84c5e58368b1
(no author) <(no author)@2a96c51b-3b00-0410-8bdb-96589899cd19>
2008-03-09 23:48:17 +0000

    Added RSpec tests for User
    git-svn-id: http://penguin-games.com/svn/gamemaster@133 2a96c51b-3b00-0410-8bdb-96589899cd19

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -3,7 +3,18 @@ require File.dirname(__FILE__) + '/../test_helper'
 class UserTest < ActiveSupport::TestCase
   should_require_attributes :email, :password_hash
   
+  def test_associations
+    assert_working_associations
+  end
+  
+  def test_has_campaigns
+    user = User.find(users(:rjordan))
+    assert user.respond_to?(:campaigns)
+  end
+  
   def test_authenticate
     assert_equal users(:rjordan), User.authenticate('rjordan@pobox.com', 'password')
   end
+  
+  
 end
