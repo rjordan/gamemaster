57786f070c5436a10f131d697866d62eb8bafe6a
Richard Kent Jordan <rjordan@pobox.com>
2009-02-11 22:44:21 -0600

    Updated to Rails 2.3

---
+++ lib/authenticated_test_helper.rb
@@ -0,0 +1,43 @@
+module AuthenticatedTestHelper
+  # Sets the current user in the session from the user fixtures.
+  def login_as(user)
+    @request.session[:user] = users(user).id
+  end
+
+  def logout
+    @request.session[:user] = nil
+  end
+
+  def current_user
+    @current_user ||= @request.session[:user] ? User.find_by_id(@request.session[:user]) : nil
+  end
+  
+  # Assert the block redirects to the login
+  # 
+  #   assert_requires_login(:bob) { get :edit, :id => 1 }
+  #
+  #def assert_requires_login(user = nil, &block)
+  #  login_as(user) if user
+  #  block.call
+  #  assert_redirected_to new_session_url
+  #end
+  def assert_requires_login(&block)
+    @request.session[:user] = nil
+    block.call  #yield?
+    assert_redirected_to new_session_url
+  end
+
+  # Assert the block accepts the login
+  # 
+  #   assert_accepts_login(:bob) { get :edit, :id => 1 }
+  #
+  # Accepts anonymous logins:
+  #
+  #   assert_accepts_login { get :list }
+  #
+  def assert_accepts_login(user = nil, &block)
+    login_as(user) if user
+    block.call
+    assert_response :success
+  end
+end
