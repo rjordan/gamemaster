8f596b19544cbc52fac47866de6f1f5dfa04167d
Richard Kent Jordan <rjordan@pobox.com>
2010-05-09 02:01:36 -0500

    added acts_as_list for chapters

--- app/models/story.rb
+++ app/models/story.rb
@@ -2,6 +2,6 @@ class Story < ActiveRecord::Base
   validates_presence_of :name, :public_description, :campaign_id
   validates_numericality_of :campaign_id
   belongs_to :campaign
-  has_many :chapters
+  has_many :chapters, :order=>'position'
   default_scope order(:name)
 end
