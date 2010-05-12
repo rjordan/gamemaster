e04ae59a832536ebb9e3504f7f2f1ba847e72404
Richard Kent Jordan <rjordan@pobox.com>
2010-05-12 17:43:13 -0500

    Silenced call to gravatar for non-production environments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,12 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def render_gravatar(user)
-    gravatar_email = Digest::MD5.hexdigest("#{user.email}")
-    "<img src='http://www.gravatar.com/avatar.php?gravatar_id=#{gravatar_email}'/>".html_safe
+    if(['production','stage'].include?(Rails.env))
+      gravatar_email = Digest::MD5.hexdigest("#{user.email}")
+      "<img src='http://www.gravatar.com/avatar.php?gravatar_id=#{gravatar_email}'/>".html_safe
+    else
+      image_tag 'ruby.png'
+    end
   end
 
   def title(title)
