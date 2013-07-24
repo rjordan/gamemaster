84b3049b2e282c95813d4a3e37074faa3da91a39
Richard Kent Jordan <rjordan@pobox.com>
2013-07-23 23:29:57 -0500

    Fixed errors on campaign_invites_controller

--- spec/controllers/campaign_invites_controller_spec.rb
+++ spec/controllers/campaign_invites_controller_spec.rb
@@ -9,47 +9,27 @@ describe CampaignInvitesController do
     @campaign = create(:campaign)
   end
 
-    #INDEX TESTS
- #   context "on GET to :index" do
- #     setup do
- #       get :index, :campaign_id=>@resource.campaign
- #     end
- #     should_respond_with :success
- #     should_not_set_the_flash
- #     #should_assign_to 'resources'
- #     should_render_template :index
- #   end
-
-    #NEW TESTS
-    context "on GET to :new" do
-      before do
-        get :new, :campaign_id=>@campaign.id
-      end
-      it { assigns(:invite).should_not be_nil }
-      #it { should_not set_the_flash }
-      it { should render_template :new }
-      it { should respond_with :success }
+  #NEW TESTS
+  context "on GET to :new" do
+    before do
+      get :new, :campaign_id=>@campaign.id
     end
+    it { assigns(:invite).should_not be_nil }
+    #it { should_not set_the_flash }
+    it { should render_template :new }
+    it { should respond_with :success }
+  end
 
-    #CREATE TESTS
-    context "on POST to :create" do
-      before do
-        post :create, campaign_id: @campaign.id, 
-                      campaign_invite: { 
-                        email: Faker::Internet.email 
-                      } 
-      end
-      it { assigns(:invite).should_not be_nil }
-      #it { should set_the_flash.to(/created/i) }
-      it { should redirect_to(campaign_path(assigns(:invite).campaign)) }
+  #CREATE TESTS
+  context "on POST to :create" do
+    before do
+      post :create, campaign_id: @campaign.id, 
+                    campaign_invite: { 
+                      email: Faker::Internet.email 
+                    } 
     end
-
-    #DELETE tests
-#    context "on DELETE to :destroy" do
-#      setup do
-#        delete :destroy, :id=>@resource
-#      end
-#      should_set_the_flash_to(/successfully removed/i)
-#      should_redirect_to("the campaign view") { campaign_path(@resource.campaign) }
-#    end
+    it { assigns(:invite).should_not be_nil }
+    #it { should set_the_flash.to(/created/i) }
+    it { should redirect_to(campaign_path(assigns(:invite).campaign)) }
+  end
 end
