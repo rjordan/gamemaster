9b9e8310812cc40374f32ca6df75f755e5829615
Richard Kent Jordan <rjordan@pobox.com>
2014-04-15 00:32:53 -0500

    Experiments in better BDD

--- spec/models/campaign_spec.rb
+++ spec/models/campaign_spec.rb
@@ -3,13 +3,13 @@ require 'spec_helper'
 describe Campaign do
   #The value of these tests is questionable
   it { expect(subject).to have_db_column(:name).of_type(:string).with_options(null: false) }
-  it { should have_db_column :user_id }
-  it { should have_db_column :system_id }
-  it { should have_db_column :max_players }
-  it { should have_db_column :public }
-  it { should have_db_column :created_at }
-  it { should have_db_column :updated_at }
-  it { should have_db_column :version }
+  it { expect(subject).to have_db_column(:user_id).of_type(:integer).with_options(null: false) }
+  it { expect(subject).to have_db_column(:system_id).of_type(:integer).with_options(null: false) }
+  it { expect(subject).to have_db_column(:max_players).of_type(:integer).with_options(null: false) }
+  it { expect(subject).to have_db_column(:public).of_type(:boolean) }
+  it { expect(subject).to have_db_column(:created_at).of_type(:datetime) }
+  it { expect(subject).to have_db_column(:updated_at).of_type(:datetime) }
+  it { expect(subject).to have_db_column(:version).of_type(:integer) }
 
   #Associations
   it { should belong_to :user }
@@ -30,13 +30,49 @@ describe Campaign do
   it { should validate_presence_of :max_players }
   it { should validate_numericality_of :max_players }
 
-  describe 'a new instance' do
+  context 'a new instance' do
     before do
       @campaign = Campaign.new
     end
-    it { expect(@campaign.public_forum).to be_instance_of(Forum) }
-    it { expect(@campaign.private_forum).to be_instance_of(Forum) }
+    describe 'public forum' do
+      it { expect(@campaign.public_forum).to be_instance_of(Forum) }
+    end
+    describe 'private forum' do
+      it { expect(@campaign.private_forum).to be_instance_of(Forum) }
+    end
   end
+
+  #BDD
+
+  context 'a user is running' do
+    before do
+      @user = create(:user)
+      @campaign = create(:campaign, user: @user)
+    end
+    describe '#game_master?' do
+      it { expect(@campaign.game_master?(@user)).to be_true }
+    end
+    describe '#player?' do
+      it { expect(@campaign.player?(@user)).to be_false }
+    end
+  end
+
+  context 'a user is playing' do
+    before do
+      @user = create(:user)
+      @character = create(:character, user: @user)
+      @campaign = create(:campaign, player_characters: [@character])
+    end
+    describe '#game_master?' do
+      it { expect(@campaign.game_master?(@user)).to be_false }
+    end
+    describe '#player?' do
+      it { expect(@campaign.player?(@user)).to be_true }
+    end
+  end
+
+
+  #TDD
 
   describe '#game_master?' do
     context 'when false' do
