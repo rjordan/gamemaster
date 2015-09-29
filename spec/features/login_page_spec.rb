91336a6440217f3ce434d1c4436aa1bb153b4e65
Richard Kent Jordan <rjordan@pobox.com>
2015-09-28 21:11:15 -0500

    Fixed login spec

--- spec/features/login_page_spec.rb
+++ spec/features/login_page_spec.rb
@@ -6,7 +6,7 @@ describe 'The login page', type: :feature do
       visit '/users/login'
       fill_in 'Email', with: 'nobody@example.org'
       fill_in 'Password', with: 'notapassword'
-      click_button 'Sign in'
+      click_button 'Log in'
     end
     it { expect(page).to have_content('Invalid email or password.') }
   end
@@ -16,7 +16,7 @@ describe 'The login page', type: :feature do
       visit '/users/login'
       fill_in 'Email', with: @valid_user.email
       fill_in 'Password', with: 'Password1'
-      click_button 'Sign in'
+      click_button 'Log in'
     end
     it { expect(page).to have_content('Signed in successfully.') }
   end
